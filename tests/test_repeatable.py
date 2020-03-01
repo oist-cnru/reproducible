e92a1a19c16964d2c7c2d2f17fc71d10a9086a2c
Fabien C. Y. Benureau <fabien@benureau.com>
2020-03-01 19:08:17 +0900

    fix cpuinfo making test unstable

--- tests/test_repeatable.py
+++ tests/test_repeatable.py
@@ -3,15 +3,25 @@ import time
 import reproducible
 
 
+def _scrub_cpu_info(data):
+    """Remove data from cpu info that is not reproducible"""
+    data['cpuinfo'].pop('hz_actual', None)
+    data['cpuinfo'].pop('hz_actual_raw', None)
+    data['cpuinfo'].pop('hz_advertised', None)
+    data['cpuinfo'].pop('hz_advertised_raw', None)
+
 def test_repeatable():
     """Check that executions of walk with identical parameters yield the same results."""
     for path in ('.', None):
-        git_data_1 = reproducible.Context().data
-        time.sleep(0.01)
-        git_data_2 = reproducible.Context().data
+        git_data_1 = reproducible.Context(cpuinfo=False).data
+        #time.sleep(0.01)
+        git_data_2 = reproducible.Context(cpuinfo=False).data
 
         timestamp_1 = git_data_1.pop('timestamp')
         timestamp_2 = git_data_2.pop('timestamp')
+        #_scrub_cpu_info(git_data_1)
+        #_scrub_cpu_info(git_data_2)
+
         assert timestamp_1 != timestamp_2
         assert git_data_1 == git_data_2
 
--- tests/test_reproducible.py
+++ tests/test_reproducible.py
@@ -1,13 +1,17 @@
 import os
 import reproducible
 
+# from test_repeatable import _scrub_cpu_info
+
 
 here = os.path.dirname(__file__)
 
 def test_sha256():
-    context = reproducible.Context()
+    context = reproducible.Context(cpuinfo=False)
     path = os.path.join(here, 'poem.txt')
     context.add_file(path, 'input')
+
+    # _scrub_cpu_info(context.data)
 
     sha256 = 'bf41d9903d19d62bdae1de79e904d361fcfa6968b7cb69c7e454d5e96200b85d'
     assert context.data['files']['input'][path]['sha256'] == sha256
